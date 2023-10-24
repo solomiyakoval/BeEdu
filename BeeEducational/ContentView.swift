a3762c076b9e2e91ef6f90912a05c90e96b21913
SkyRed003 <francescopalumbo574@gmail.com>
2023-10-24 10:52:04 +0200

    WelcomeScreen

--- BeeEducational/ContentView.swift
+++ BeeEducational/ContentView.swift
@@ -10,46 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var Player: String = "Gino"
-    var Points: String = "2874365"
     var body: some View {
-        
-        
-        ZStack {Color(.yellow)
-                .opacity(0.16)
-                .background()
-                .ignoresSafeArea()
-            
-            VStack {
-               
-                
-                
-                Text("Welcome back")
-                    .fontWeight(.bold)
-                    .font(.largeTitle)
-                    .foregroundStyle(.yellow)
-                Text("\(Player)!")
-                    .fontWeight(.bold)
-                    .font(.largeTitle)
-                    .foregroundStyle(.yellow)
-                
-                
-                
-                
-                Text("Let's keep going on cleaning the world")
-                    .bold()
-                
-                
-                Text("Tap to continue..")
-                    .padding(.top, 300)
-                
-                
-            }
-            .padding().frame(width: 500)
-        }
+        Text("oi")
     }
+    
 }
-
 #Preview {
     ContentView()
 }
