cf765204894dadf732e641d59d192ca0cea2ed94
solomiyakoval <148435486+solomiyakoval@users.noreply.github.com>
2023-10-24 13:15:05 +0200

    added correct and wrong answers

--- BeeEducational/CorrectAnswerView.swift
+++ BeeEducational/CorrectAnswerView.swift
@@ -8,31 +8,67 @@
 import SwiftUI
 
 struct CorrectAnswerView: View {
-    var body: some View {
-        
-        
-        let questionViewModel = QuestionViewModel()
-        
-        
-        ZStack {
+        var body: some View {
             
-            Image("background")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .ignoresSafeArea()
             
-            VStack {
-            }
-            .frame(width: 390, height: 844)
-            .overlay(
-                Rectangle()
-                    .inset(by: -5)
-                    .stroke(.black.opacity(0), lineWidth: 10)
-                    .padding()
+            let questionViewModel = QuestionViewModel()
+            
+            
+            ZStack {
                 
-        )
+                Image("background")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    
+                    Text ("You are right!")
+                    
+                        .font(.system(size: 55))
+                        .bold()
+                        
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color(red: 0.53, green: 0.8, blue: 0.36))
+                        .padding(20)
+                
+                    
+                    Text(" Correct answer is:")
+                        .font(.system(size: 30))
+                        
+                    
+                        .multilineTextAlignment(.center)
+                        .foregroundColor (.black)
+                        .padding(5)
+                    
+                    
+                    Text(questionViewModel.questions[0].correctanswer)
+                        .font(.system(size: 25))
+                        .bold()
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.black)
+                        .frame(width: 311, height: 41, alignment: .top)
+                        .background(Rectangle().fill(Color (red: 0.53, green: 0.8, blue: 0.36)).shadow(radius: 3))
+                        .cornerRadius(40)
+                        .padding(10)
+                    
+                    
+                    
+                    
+                    
+                    
+                }
+                .frame(width: 390, height: 844)
+                .overlay(
+                    Rectangle()
+                        .inset(by: -5)
+                        .stroke(.black.opacity(0), lineWidth: 10)
+                        .padding()
+                    
+            )
+            }
         }
-    }
+    
 }
 
 #Preview {
